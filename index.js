30457fd5e87d3734dc5b1588105548315d5d5ee3
Brett Byrd <byrdb2@yahoo.com>
2023-02-22 14:34:07 -0600

    complete

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting
+  
+  if (feet <= 400){
+   return greeting = 'This one is on me!'
+  
+  }
+
+  
+else if (feet > 400 && feet < 2000){
+  return greeting = 'That will be twenty bucks.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (feet > 2000 && feet < 2500) {
+ return greeting = 'I will gladly take your thirty bucks.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+ 
+else  (feet > 2500)
+  return greeting = 'No can do.'
+}
+function ternaryCheckCity(city){
+return city === 'NYC'?'Ok, sounds good.':'No go.'
+}
+function switchOnCharmFromTip(tip){
+ switch(tip) {
+  case 'generous' :
+    return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+      default:
+        return 'Bye.'
+ } 
 }
